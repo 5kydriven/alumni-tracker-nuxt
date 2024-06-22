28f7aff5aa0706925d231550ab6da32a09d343ec
5kydriven <tagpuno619@gmail.com>
2024-06-22 10:39:06 +0800

    few changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "nuxt-vuefire"],
   colorMode: {
     preference: 'light'
-  }
+  },
+  vuefire: {
+    auth: {
+      enabled: true
+    },
+    config: {
+      apiKey: process.env.apiKey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId,
+      // there could be other properties depending on the project
+    },
+  },
 })
